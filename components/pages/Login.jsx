c9a9e070ac61f9462177c3782f3c55499906a88a
cesar jesus leon martin <cesilius98@gmail.com>
2023-02-13 14:36:11 +0100

    Feature: login screen initial styles

--- components/pages/Login.jsx
+++ components/pages/Login.jsx
@@ -1,14 +1,68 @@
 
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, Pressable} from 'react-native'
+import {useState} from 'react'
+
+
+
 
 const Login = () =>{
+
+
+
     return(
-        <View>
-            <Text>
-                LOGIN
-            </Text>
+        <View style={styles.containerLogin}>
+            <Text style={styles.titleLogin}>BIENVENIDO A SPACETRADER</Text>
+            <View style={styles.buttonBox}>
+                <Pressable style={styles.loginButton}>
+                    <Text style={styles.textButton}>Login</Text>
+                </Pressable>
+                <Pressable style={styles.registerButton}>
+                    <Text style={styles.textButton}>
+                        Register
+                    </Text>
+                </Pressable>
+            </View>
+            
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    containerLogin:{
+        width:"100%",
+        height:"100%",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    loginButton:{
+        width:"60%",
+        height:"10%",
+        backgroundColor:"#6AA0EE"
+    },
+    registerButton:{
+        width:"60%",
+        height:"10%",
+        backgroundColor:"#FCC02D"
+    },
+    textButton:{
+        height:"100%",
+        width:"100%",
+        textAlign:"center",
+        textAlignVertical:"center",
+        color:"white",
+        fontWeight:"bold"
+    },
+    buttonBox:{
+        justifyContent:"center",
+        alignItems:"center",
+        height:"50%",
+        width:"50%"
+    },
+    titleLogin:{
+        fontSize:30,
+        fontWeight:"bold",
+        textAlign:"center"
+    }
+
+})
 export default Login
